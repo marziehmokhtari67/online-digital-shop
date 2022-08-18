d8f39af79a78981d3c31d5376ebfe854287fd605
marziehmokhtari <marziehmokhtari67@gmail.com>
2022-08-18 22:52:39 +0430

    UI:creating table row and table cells

--- src/Component/Goods/Row.js
+++ src/Component/Goods/Row.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-function Row() {
+import {TableRow,TableCell,Button,Avatar} from '@mui/material'
+
+const URL='http://localhost:3001'
+function Row({post,category}) {
+
   return (
-    <div>
+    <>
+    <TableRow>
+   
+        <TableCell align='right' sx={{fontFamily:'iran'}}><Avatar alt="image" src={`${URL}/files/${post.thumbnail}`} /></TableCell>
+        <TableCell align='right' sx={{fontFamily:'iran'}}>{post.name}</TableCell>
+        <TableCell align='right' sx={{fontFamily:'iran'}}>{post.category}</TableCell>
+        <TableCell align='right' ><Button variant='outlined' sx={{fontFamily:'iran'}}>ویرایش</Button><Button variant='outlined' sx={{fontFamily:'iran'}}>حذف</Button></TableCell>
+      </TableRow>
       
-    </div>
+    </>
   )
 }
 
