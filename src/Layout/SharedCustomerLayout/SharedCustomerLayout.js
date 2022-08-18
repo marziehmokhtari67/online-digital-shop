106ae6130ef33dae13136f9a5f2ff1a7832bd84a
marziehmokhtari <marziehmokhtari67@gmail.com>
2022-08-18 20:51:02 +0430

    first

--- src/Layout/SharedCustomerLayout/SharedCustomerLayout.js
+++ src/Layout/SharedCustomerLayout/SharedCustomerLayout.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-import Box from '@mui/material/Box'
+import CustomerNavbar from '../../Component/CustomerNavbar'
 function SharedCustomerLayout() {
   return (
     <>
-     <Box
-      sx={{
-        width: 1,
-        height:1/5,
-        backgroundColor:'#f3e5f5', 
-        border:'1px solid black',
-        padding:'30px',
-        textAlign:'center',
-       
-      }}
-    >
-      SharedCustomerLayout
-    </Box>
-      {/* <NavBar></NavBar> */}
+     
+     <CustomerNavbar/>
+  
       <Outlet/>
     </>
   )
